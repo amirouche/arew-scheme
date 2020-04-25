7aa098bac068d339e378651385fcc23ede97ce37
Amirouche <amirouche.boubekki@gmail.com>
2020-04-25 15:01:00 +0200

    tests++

--- src/tests-tests.scm
+++ src/tests-tests.scm
@@ -1,9 +1,15 @@
 (library (tests-tests)
 
-  (export test-0)
+  (export test-0
+          test-1)
 
   (import (chezscheme)
           (tests))
 
   (define test-0
-    (test #t #t)))
+    (test #t #t))
+
+  (define test-1
+    (test #f #f))
+
+  )
